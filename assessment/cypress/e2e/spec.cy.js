148d617a64ef7243ad4259d30a84b1e3fc8187a8
aagamjain04 <4ni19cs001_a@nie.ac.in>
2025-01-03 17:24:43 +0530

    Initial commit

---
+++ assessment/cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+// CRIO_SOLUTION_START_MODULE_ONE
+// CRIO_SOLUTION_END_MODULE_ONE
+describe('Bot AI Page', () => {
+  beforeEach(() => {
+    //cy.visit('http://localhost:5500/notifications-page-main/index.html'); // Adjust the path to your HTML file
+  });
+});
---
+++ src/theme/ThemeContext.jsx
@@ -0,0 +1,5 @@
+// CRIO_SOLUTION_START_MODULE_ONE
+import { createContext } from 'react'
+
+export const ThemeContext = createContext('light')
+// CRIO_SOLUTION_END_MODULE_ONE
